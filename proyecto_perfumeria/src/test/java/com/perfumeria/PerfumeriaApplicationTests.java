82241c60bd07eef75e17905c6c2fac5e1026ecba
Brenda <bperez37859@ufide.ac.cr>
2026-03-13 10:03:01 -0600

    1

---
+++ proyecto_perfumeria/src/test/java/com/perfumeria/PerfumeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfumeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
